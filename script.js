f808af63da5d50dace2242d884456619255d5943
Daniel Djupvik Sætre <danieldjupvik@Daniels-MacBook-Pro.local>
2020-09-22 18:40:32 +0200

    finished

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const games = [
-    { id: 324, name: "Great game", rating: 5.0 },
-    { id: 124, name: "Ok game", rating: 3.0 },
-    { id: 678, name: "Meh game", rating: 2.5 },
-    { id: 28, name: "Terrible game", rating: 1.0 },
-    { id: 4, name: "Mediocre game", rating: 3.0 },
+  { id: 324, name: "Great game", rating: 5.0 },
+  { id: 124, name: "Ok game", rating: 3.0 },
+  { id: 678, name: "Meh game", rating: 2.5 },
+  { id: 28, name: "Terrible game", rating: 1.0 },
+  { id: 4, name: "Mediocre game", rating: 3.0 },
 ];
+
+const resultContainer = document.querySelector(".result-container");
+
+games.forEach((games) => {
+  resultContainer.innerHTML += `
+    <ul class="card" >
+        <li>Game: ${games.name}</li>
+        <li>Rating: ${games.rating}</li>
+    </ul>`;
+});
+
+const card = document.querySelectorAll(".card");
+
+card.forEach((cards) => {
+  cards.addEventListener("click", (event) => {
+    event.target.classList.toggle("played");
+  });
+});
